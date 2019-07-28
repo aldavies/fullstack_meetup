4d1770bffb91611e0eccda0782ab75ef434f9db8
aldavies <aldavies@umich.edu>
2019-07-28 00:23:07 -0400

    example fetch from backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 8000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => res.json("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:8000/")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ data });
+        console.log(this.state);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
