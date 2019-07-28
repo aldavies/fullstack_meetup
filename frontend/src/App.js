2adefacc9ee3ef1fb6632d8d5684dfe5f2bebfe6
aldavies <aldavies@umich.edu>
2019-07-28 00:29:03 -0400

    changed welcome message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,9 +24,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Open console and see data</p>
           <a
             className="App-link"
             href="https://reactjs.org"
