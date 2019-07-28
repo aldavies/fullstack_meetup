f85dc41b0b3ade97b9d1fffb93743c3c393f6d52
jtx007 <jamesjacobthomas7@gmail.com>
2019-07-28 00:58:14 -0400

    preliminary setup for react router, homepage, and general about [age

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import HomePage from './components/HomePage'
+import AboutPage from './components/AboutPage'
+import history from './history'
+/* 
+Created seperate history object for programatic rerouting throughtout application
+and then passed into Router component
+*/
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router history={history}>
+          < Navbar/>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/about" component={AboutPage} />
+          </Switch>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1 className="main-header">Welcome to Full Stack Coding</h1>
+        </div>
+    )
+}
+
+export default HomePage
